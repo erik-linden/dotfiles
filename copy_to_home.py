0767b9ee5ee6b1d7325d558c129aeda4b925b7c3
Erik Lindén <erlinden@kth.se>
2018-05-10 16:12:19 +0200

    Don't print overlapping blocks

--- copy_to_home.py
+++ copy_to_home.py
@@ -65,22 +65,18 @@ def _interactive_patch(src_lines, dst_lines):
         i = indices.pop(0)
         if diff[i][:2] != "  ":  # new block
             # roll up the block
-            block_begin = i
+            begin = i
             while indices:
                 line = diff[indices[0]]
                 if line[:2] != "  ":
                     i = indices.pop(0)
                 else:  # block ended
                     break
-            block_end = i + 1
+            end = i + 1
 
             # decide what to do
             while True:
-                print("".join(80 * ["#"]))
-                display_begin = max(0, block_begin - _PAD)
-                display_end = min(len(diff), block_end + _PAD)
-                for l in diff[display_begin:display_end]:
-                    print(l, end="")
+                _print_block(diff, begin, end)
 
                 key = input("Apply change [y, n, q]? ").lower().strip()
 
@@ -91,7 +87,7 @@ def _interactive_patch(src_lines, dst_lines):
                     sys.exit()
 
                 which = {"n": 1, "y": 2}[key]
-                restored = list(difflib.restore(diff[block_begin:block_end], which))
+                restored = list(difflib.restore(diff[begin:end], which))
                 output.extend(restored)
 
                 break
@@ -100,6 +96,13 @@ def _interactive_patch(src_lines, dst_lines):
             output.append(diff[i][2:])
 
     return output
+
+
+def _print_block(diff, begin, end):
+    print("".join(80 * ["#"]))
+    [print(l, end="") for l in diff[max(0, begin - _PAD): begin + 1] if l[:2] == "  "]
+    [print(l, end="") for l in diff[begin: end]]
+    [print(l, end="") for l in diff[end: min(len(diff), end + _PAD + 1)] if l[:2] == "  "]
 
 
 if __name__ == "__main__":
