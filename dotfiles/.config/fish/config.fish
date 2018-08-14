9780baf20f42c27947b40fd7f681972431995758
Erik Lindén <erlinden@kth.se>
2018-08-14 18:44:46 +0200

    Support `conda activate` in fish

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -4,8 +4,8 @@ set fish_greeting
 set PATH /usr/local/cuda-9.1/bin $PATH
 set LD_LIBRARY_PATH /usr/local/cuda-9.1/lib64 $LD_LIBRARY_PATH
 
-# added by Miniconda3 installer
-set PATH /home/tobii.intra/elin/miniconda3/bin $PATH
+# enable `conda activate`
+source /home/tobii.intra/elin/miniconda3/etc/fish/conf.d/conda.fish
 
 # powerline
 set fish_function_path $fish_function_path /usr/share/powerline/bindings/fish
