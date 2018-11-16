08185a8ec07d7765f962faee307523348ae1914d
Erik Lindén <erlinden@kth.se>
2018-11-16 14:35:14 +0100

    Little tweaks

--- dotfiles/.config/fish/functions/fish_user_key_bindings.fish
+++ dotfiles/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,16 @@
 function fish_user_key_bindings
-    # like vi, but keep things like ctrl-f
+    # Like vi, but keep things like ctrl-f
     fish_hybrid_key_bindings
+    # Use jj and jk as escape keys
+    bind -M insert jj fish_escape
+    bind -M insert jk fish_escape
+end
+
+function fish_escape
+    if commandline -P
+        commandline -f cancel
+    else
+        set fish_bind_mode default
+        commandline -f backward-char force-repaint
+    end
 end
