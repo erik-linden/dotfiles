f3af2bf3111c633e6eac4dc7538c13b76be05688
Erik Lindén <erlinden@kth.se>
2018-12-08 11:46:38 +0100

    Export path

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,12 +1,6 @@
 # empty greeting
 set fish_greeting
 
-# https://unix.stackexchange.com/questions/230238/starting-x-applications-from-the-terminal-and-the-warnings-that-follow
-set NO_AT_BRIDGE 1
-
-# conda python, append instead of prepend to keep system interpreter
-set PATH $PATH /home/tobii.intra/elin/miniconda3/bin
-
 # enable `conda activate`
 source /home/tobii.intra/elin/miniconda3/etc/fish/conf.d/conda.fish
 
@@ -14,6 +8,12 @@ source /home/tobii.intra/elin/miniconda3/etc/fish/conf.d/conda.fish
 set fish_function_path $fish_function_path /usr/share/powerline/bindings/fish
 powerline-setup
 
+# https://unix.stackexchange.com/questions/230238/starting-x-applications-from-the-terminal-and-the-warnings-that-follow
+set -x NO_AT_BRIDGE 1
+
+# conda python, append to keep system interpreter
+set -x PATH $PATH /home/tobii.intra/elin/miniconda3/bin
+
 # setup cuda paths
-set PATH /usr/local/cuda-9.1/bin $PATH
-set LD_LIBRARY_PATH /usr/local/cuda-9.1/lib64 $LD_LIBRARY_PATH
+set -x PATH /usr/local/cuda-9.1/bin $PATH
+set -x LD_LIBRARY_PATH /usr/local/cuda-9.1/lib64 $LD_LIBRARY_PATH
