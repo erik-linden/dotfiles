ded936207c23a09af75c04703e1b0b98b12cfc49
Erik Lindén <elin@tobii.com>
2020-04-27 15:31:54 +0200

    Fix fish conda prompt problem

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,12 +1,6 @@
 # empty greeting
 set fish_greeting
 
-# powerline
-set POWERLINE_CONFIG_COMMAND ~/miniconda3/bin/powerline-config
-set POWERLINE_COMMAND ~/miniconda3/bin/powerline
-set fish_function_path $fish_function_path ~/miniconda3/lib/python3.7/site-packages/powerline/bindings/fish
-powerline-setup
-
 # https://unix.stackexchange.com/questions/230238/starting-x-applications-from-the-terminal-and-the-warnings-that-follow
 set -x NO_AT_BRIDGE 1
 
@@ -17,3 +11,9 @@ xmodmap -e 'keycode 49 = asciitilde'
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/tobii.intra/elin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
+
+# powerline, must be after conda, or conda will overwrite 'fish_left_prompt'
+set POWERLINE_CONFIG_COMMAND ~/miniconda3/bin/powerline-config
+set POWERLINE_COMMAND ~/miniconda3/bin/powerline
+set fish_function_path $fish_function_path ~/miniconda3/lib/python3.7/site-packages/powerline/bindings/fish
+powerline-setup
